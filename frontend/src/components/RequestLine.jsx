f1fd99b64a48752c1d3c236567760b3599f4b32f
CameronBibb <cameron.bibb@icloud.com>
2026-02-02 12:20:09 -0500

    refactor: update RequestLine component styles and structure for better readability

--- frontend/src/components/RequestLine.jsx
+++ frontend/src/components/RequestLine.jsx
@@ -2,7 +2,7 @@ import helpers from "../services";
 import "./RequestLine.css";
 
 const RequestLine = ({ request, setSelectedRequest }) => {
-  const path = helpers.removeBinFromPath(request.http_path);
+  // const path = helpers.removeBinFromPath(request.http_path);
   const method = request.http_method;
   const time = request.received_at;
 
@@ -22,7 +22,10 @@ const RequestLine = ({ request, setSelectedRequest }) => {
   return (
     <a href="#" onClick={onClick}>
       <li className="request_line">
-        {convertDbTimetoDateObj(time).toLocaleTimeString()} {method} {path}
+        <span className="request_line_time">
+          {convertDbTimetoDateObj(time).toLocaleTimeString()}
+        </span>{" "}
+        <span className="request_line_method">{method}</span>
       </li>
     </a>
   );
